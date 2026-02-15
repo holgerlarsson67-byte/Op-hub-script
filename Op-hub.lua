ac5460b820e0e02d9f7f74df5f36a7d2cf471a3c
holgerlarsson67-byte <holgerlarsson67@gmail.com>
2026-02-15 23:06:13 +0100

    Op-hub.lua

--- Op-hub.lua
+++ Op-hub.lua
@@ -45,23 +45,7 @@ local AntiRagdollEnabled = false
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
-local DashPadsFolder = workspace:WaitForChild("DashPads")
 
-local function isTouchingDashPad()
-	local character = LocalPlayer.Character
-	if not character then return false end
-
-	local hrp = character:FindFirstChild("HumanoidRootPart")
-	if not hrp then return false end
-
-	for _, part in ipairs(hrp:GetTouchingParts()) do
-		if part:IsDescendantOf(DashPadsFolder) then
-			return true
-		end
-	end
-
-	return false
-end
 
 local function disableRagdollRemotes()
 	local serverRemotes = ReplicatedStorage:FindFirstChild("Remotes")
@@ -552,13 +536,22 @@ task.spawn(function()
 	end
 end)
 
+local touching = false
+
 task.spawn(function()
 	while true do
-		if isTouchingDashPad() and AntiRagdollEnabled then
-			EnableRagdollRemotes()
-			task.wait(0.5)
+		local currentlyTouching = isTouchingSpawn()
+
+		if currentlyTouching and not touching and AntiRagdollEnabled then
+			touching = true
 			disableRagdollRemotes()
+			print("Started touching SpawnArea")
+		elseif not currentlyTouching and touching and not AntiRagdollEnabled then
+			touching = false
+			EnableRagdollRemotes()
+			print("stopped touching SpawnArea")
 		end
+
 		task.wait(0.05)
 	end
 end)
